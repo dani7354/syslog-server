f357c991581acca3e7e6b9155c621ad8b6cbd4b5
Daniel <d@stuhrs.dk>
2023-10-30 17:42:26 +0100

    Add check to connect script

--- ssh-tunnel/connect_to_hosts.sh
+++ ssh-tunnel/connect_to_hosts.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 RSYSLOG_HOST="rsyslog"
-REMOTE_PORT="5514"
+REMOTE_PORT="8345"
 SSH_DIR="/opt/ssh_tunnel/.ssh"
 SSH_HOSTS_LIST_FILE="${SSH_DIR}/host_list"
 SSH_KNOWN_HOSTS_PATH="${SSH_DIR}/known_hosts"
 SSH_CONFIG_PATH="${SSH_DIR}/config"
 
-readarray -t hosts < "$SSH_HOSTS_LIST_FILE" 
+[[ -r "$SSH_HOSTS_LIST_FILE"  ]] || { echo "SSH host list not found!" >&2; exit 1; }
+[[ -r "$SSH_KNOW_HOSTS_PATH"  ]] || { echo "SSH known hosts file not found!" >&2; exit 1; }
+[[ -r "$SSH_CONFIG_PATH"  ]] || { echo "SSH config not found!" >&2; exit 1; }
+
+readarray -t hosts < "$SSH_HOSTS_LIST_FILE"
 
 for host in "${hosts[@]}"; do
     command="ssh -F ${SSH_CONFIG_PATH} -NfT -o UserKnownHostsFile=${SSH_KNOWN_HOSTS_PATH} -R ${REMOTE_PORT}:${RSYSLOG_HOST}:514 ${host}"
